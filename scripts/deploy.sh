bee62e5cd6f2a634fe584c103ddb7b006a8cc3c1
HuiYun <102589253+Tldkt@users.noreply.github.com>
2023-01-15 23:23:52 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,30 +1,24 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-PROJECT_NAME=fitback-security
-BUILD_JAR=$(ls /home/ubuntu/action/build/libs/*.jar)
-JAR_NAME=$(basename $BUILD_JAR)
+REPOSITORY=/home/ubuntu/server
+cd $REPOSITORY
 
-echo "> 현재 시간: $(date)" >> /home/ubuntu/action/deploy.log
+APP_NAME=fitback-security
+JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
+JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
-echo "> build 파일명: $JAR_NAME" >> /home/ubuntu/action/deploy.log
+CURRENT_PID=$(pgrep -f $APP_NAME)
 
-echo "> build 파일 복사" >> /home/ubuntu/action/deploy.log
-DEPLOY_PATH=/home/ubuntu/action/
-cp $BUILD_JAR $DEPLOY_PATH
-
-echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ubuntu/action/deploy.log
-CURRENT_PID=$(pgrep -f $JAR_NAME)
-
-if [ -z $CURRENT_PID ]
+if [ -z "$CURRENT_PID" ]
 then
-  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ubuntu/action/deploy.log
+  echo "> 종료할 것 없음." >> /home/ubuntu/deploy.log
 else
-  echo "> kill -9 $CURRENT_PID" >> /home/ubuntu/action/deploy.log
-  sudo kill -9 $CURRENT_PID
+  echo "> sudo kill -15 $CURRENT_PID" >> /home/ubuntu/deploy.log
+  kill -15 "$CURRENT_PID" >> /home/ubuntu/deploy.log 2>&1
   sleep 5
 fi
 
+echo "> $JAR_PATH 배포"
+nohup java -jar -Dspring.config.location=classpath:/,file:///home/ubuntu/study-app-env/application-prod.properties "$JAR_PATH" >> /home/ubuntu/deploy.log 2>&1 &
 
-DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
-echo "> DEPLOY_JAR 배포"    >> /home/ubuntu/action/deploy.log
-sudo nohup java -jar $DEPLOY_JAR >> /home/ubuntu/deploy.log 2>/home/ubuntu/action/deploy_err.log &
+echo "[$(date)] server deploy" >> /home/ubuntu/deploy.log
