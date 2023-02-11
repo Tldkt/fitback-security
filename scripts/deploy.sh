5f1927a09982d99954973dbc864cb9ab28e92281
HuiYun <102589253+Tldkt@users.noreply.github.com>
2023-02-11 20:43:19 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,24 +1,23 @@
 #!/usr/bin/env bash
 
 REPOSITORY=/home/ubuntu/server
-cd $REPOSITORY
+cd $REPOSITORY || exit
 
 APP_NAME=fitback-security
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
-CURRENT_PID=$(pgrep -f $APP_NAME)
-
-if [ -z "$CURRENT_PID" ]
-then
-  echo "> 종료할 것 없음." >> /home/ubuntu/deploy.log
+echo "Stopping the current instance of $APP_NAME if it is running..."
+CURRENT_PID=$(pgrep -f "$APP_NAME")
+if [ -n "$CURRENT_PID" ]; then
+echo "Killing process with PID $CURRENT_PID"
+kill -15 "$CURRENT_PID"
+sleep 5
 else
-  echo "> sudo kill -15 $CURRENT_PID" >> /home/ubuntu/deploy.log
-  kill -15 "$CURRENT_PID" >> /home/ubuntu/deploy.log 2>&1
-  sleep 5
+echo "No process found to stop."
 fi
 
-echo "> $JAR_PATH 배포"
+echo "Starting new instance of $APP_NAME..."
 nohup java -jar -Dspring.config.location=classpath:/,file:///home/ubuntu/study-app-env/application-prod.properties "$JAR_PATH" >> /home/ubuntu/deploy.log 2>&1 &
 
-echo "[$(date)] server deploy" >> /home/ubuntu/deploy.log
+echo "[$(date)] Deployment of $APP_NAME completed" >> /home/ubuntu/deploy.log
